ca26903003725be5ca92a6a5682e08bfb0b9d382
ThevinduW <itsthw9@gmail.com>
2025-05-07 16:16:14 +0530

    Initialize

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 5003;
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+  console.log("RS.");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
